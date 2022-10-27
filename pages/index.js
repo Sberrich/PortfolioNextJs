b4cc63be5d69a37bbbbce9fc62942bdd4042a76c
Berrichi Samir <49416974+Sberrich@users.noreply.github.com>
2022-10-27 09:59:45 +0100

    Last commit

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,12 @@ import wave from "../public/wave.png";
 import design from "../public/design.png";
 import code from "../public/code.png";
 import consulting from "../public/consulting.png";
-import web1 from "../public/web1.png";
-import web2 from "../public/web2.png";
-import web3 from "../public/web3.png";
-import web4 from "../public/web4.png";
-import web5 from "../public/web5.png";
-import web6 from "../public/web6.png";
+// import web1 from "../public/web1.png";
+// import web2 from "../public/web2.png";
+// import web3 from "../public/web3.png";
+// import web4 from "../public/web4.png";
+// import web5 from "../public/web5.png";
+// import web6 from "../public/web6.png";
 import { useState } from "react";
 import Link from "next/link";
 
@@ -31,7 +31,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className=" bg-white px-10 md:px20 lg:px-40 dark:bg-gray-900">
+      <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between dark:text-white">
             <h1 className="text-xl font-burtons">Sberrich</h1>
@@ -44,23 +44,24 @@ export default function Home() {
               </li>
               <li>
                 <a
-                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white dark:bg-gray-900 px-4 py-2 rounded-md ml-8"
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white dark:bg-gray-900 px-4 py-2 rounded-md ml-8 text-l font-burtons"
                   href="#">
-                  Resume
+                  Say Hello
                 </a>
               </li>
             </ul>
           </nav>
           <div className="text-center p-10 py-10">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl text-xl font-burtons">
               Berrichi Samir
             </h2>
-            <h3 className="text-2xl py-2 md:text-xl dark:text-white">
-              Frontend developer
+            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
+              Front-end Developer
             </h3>
-            <p className="text-md py-5 leading-8 text-gray-800  md:text-xl max-w-lg mx-auto">
-              {/* Freelancer providing services for programming and web dev, Join me
-              down below and l`ets get cracking! */}
+            <p className="text-md py-5 leading-8 text-gray-800  md:text-l max-w-lg mx-auto  dark:text-gray-200 ">
+              I design and code beautifully simple things, and i love what i do,
+              Freelancer providing services for programming and web dev, Join me
+              down below and lets get cracking!
             </p>
           </div>
           <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
@@ -73,49 +74,54 @@ export default function Home() {
             <Link href="https://github.com/Sberrich">
               <AiFillGithub className="cursor-pointer" />
             </Link>
+            <Link href="https://instagram.com/berrichi_samir">
+              <AiFillInstagram className="cursor-pointer" />
+            </Link>
           </div>
           <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
             <Image src={wave} layout="fill" objectFit="cover" />
           </div>
           <section>
             <div>
-              <h3 className="text-3xl py-1 dark:text-white">
-                Services I offer
+              <h3 className="text-3xl py-1 dark:text-white font-burtons">
+              Welcome.
               </h3>
-              <p className="text-md py-2 leading-8 text-gray-800 ">
-                Freelancer providing services for programming and web dev, Join
-                me down below and l`ets get cracking!
+              <p className="text-md py-2 leading-8 text-gray-800  dark:text-gray-200 ">
+                My name is Samir Berrichi, I'm a Front-end developer based in khouribga Morocco, i have been developed many types of front-ends from well know Matcha "Matching and chating app", hypetype "streaming app for watching movies"
+              </p>
+              <p className="text-md py-2 leading-8 text-gray-800   dark:text-gray-200">
+                I'm passionate about creating a beautiful interfaces and intuitively implemanted UX
               </p>
             </div>
             <div className="lg:flex gap-10">
-              <div className="text-center shadow-lg p-10 rounded-xl my-12">
+              <div className="text-center shadow-lg p-10 rounded-xl my-12 ">
                 <Image src={design} width={100} height={100} />
-                <h3>Beautiful Designs</h3>
-                <p>Creating elegant designs suited for your design theory</p>
-                <h4 className="py-4 text-teal-600">Design tools I use</h4>
-                <p className="text-gray-800 py-1">Figma</p>
-                <p className="text-gray-800 py-1">Ullistrator</p>
+                <h3 className="font-burtons  dark:text-white">Beautiful Designs</h3>
+                <p className=" dark:text-gray-200">Creating elegant designs suited for your design theory</p>
+                <h4 className="py-4 text-teal-600  dark:text-gray-200">Design tools I use</h4>
+                <p className="text-gray-800 py-1  dark:text-gray-200">Figma</p>
+               
               </div>
               <div className="text-center shadow-lg p-10 rounded-xl my-12">
                 <Image src={code} width={100} height={100} />
-                <h3>Beautiful Projects</h3>
-                <p>Creating designs suited for your design theory</p>
-                <h4 className="py-4 text-teal-600">Design tools I use</h4>
-                <p className="text-gray-800 py-1">Figma</p>
-                <p className="text-gray-800 py-1">Ullistrator</p>
+                <h3 className="font-burtons  dark:text-white">Frontend Dev</h3>
+                <p className=" dark:text-gray-200">Creating and integrating UI</p>
+                <h4 className="py-4 text-teal-600  dark:text-gray-200"> I use</h4>
+                <p className="text-gray-800 py-1  dark:text-gray-200">Tailwind</p>
+                <p className="text-gray-800 py-1  dark:text-gray-200">Mui</p>
               </div>
-              <div className="text-center shadow-lg p-10 rounded-xl my-12">
+              <div className="text-center shadow-lg p-10 rounded-xl my-12  dark:text-gray-200">
                 <Image src={consulting} width={100} height={100} />
-                <h3>Beautiful Designs</h3>
+                <h3 className="font-burtons  dark:text-white">libraries and frameworks</h3>
                 <p>Creating elegant designs suited for your design theory</p>
-                <h4 className="py-4 text-teal-600">Design tools I use</h4>
-                <p className="text-gray-800 py-1">Figma</p>
-                <p className="text-gray-800 py-1">Ullistrator</p>
+                <h4 className="py-4 text-teal-600  dark:text-gray-200">Tools I use</h4>
+                <p className="text-gray-800 py-1   dark:text-gray-200">ReactJs</p>
+                <p className="text-gray-800 py-1   dark:text-gray-200">NextJs</p>
               </div>
             </div>
           </section>
         </section>
-        <section>
+        {/* <section>
           <div>
             <h3 className="text-3xl py-1">Portfolio</h3>
             <p className="text-md py-2 leading-8 text-gray-800 ">
@@ -185,7 +191,7 @@ export default function Home() {
               />
             </div>
           </div>
-        </section>
+        </section> */}
       </main>
     </div>
   );
