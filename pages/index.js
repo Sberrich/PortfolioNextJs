2cc1cfb91b76dd2cbd5f31eb23bcfb86b59e7075
Berrichi Samir <49416974+Sberrich@users.noreply.github.com>
2022-10-27 13:32:44 +0100

    update the font

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,3 @@
-
-
-//           <section>
-//             <div>
-//               <h3 className="text-3xl py-1 dark:text-white font-burtons">
-//               Welcome.
-//               </h3>
-//               <p className="text-md py-2 leading-8 text-gray-800  dark:text-gray-200 ">
-//                 My name is Samir Berrichi, I'm a Front-end developer based in khouribga Morocco, i have been developed many types of front-ends from well know Matcha "Matching and chating app", hypetype "streaming app for watching movies"
-//               </p>
-//               <p className="text-md py-2 leading-8 text-gray-800   dark:text-gray-200">
-//                 I'm passionate about creating a beautiful interfaces and intuitively implemanted UX
-//               </p>
-//             </div>
-//             <div className="lg:flex gap-10">
-//               <div className="text-center shadow-lg p-10 rounded-xl my-12 ">
-//                 <Image src={design} width={100} height={100} />
-//                 <h3 className="font-burtons  dark:text-white">Beautiful Designs</h3>
-//                 <p className=" dark:text-gray-200">Creating elegant designs suited for your design theory</p>
-//                 <h4 className="py-4 text-teal-600  dark:text-gray-200">Design tools I use</h4>
-//                 <p className="text-gray-800 py-1  dark:text-gray-200">Figma</p>
-               
-//               </div>
-//               <div className="text-center shadow-lg p-10 rounded-xl my-12">
-//                 <Image src={code} width={100} height={100} />
-//                 <h3 className="font-burtons  dark:text-white">Frontend Dev</h3>
-//                 <p className=" dark:text-gray-200">Creating and integrating UI</p>
-//                 <h4 className="py-4 text-teal-600  dark:text-gray-200"> I use</h4>
-//                 <p className="text-gray-800 py-1  dark:text-gray-200">Tailwind</p>
-//                 <p className="text-gray-800 py-1  dark:text-gray-200">Mui</p>
-//               </div>
-//               <div className="text-center shadow-lg p-10 rounded-xl my-12  dark:text-gray-200">
-//                 <Image src={consulting} width={100} height={100} />
-//                 <h3 className="font-burtons  dark:text-white">libraries and frameworks</h3>
-//                 <p>Creating elegant designs suited for your design theory</p>
-//                 <h4 className="py-4 text-teal-600  dark:text-gray-200">Tools I use</h4>
-//                 <p className="text-gray-800 py-1   dark:text-gray-200">ReactJs</p>
-//                 <p className="text-gray-800 py-1   dark:text-gray-200">NextJs</p>
-//               </div>
-//             </div>
-//           </section>
-//         </section>
-
-
-
 import Head from "next/head";
 import {
   AiFillTwitterCircle,
@@ -93,7 +48,7 @@ export default function Home() {
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl font-burtons">
              SAMIR BERRICHI
             </h2>
-            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
+            <h3 className="text-2xl py-2 dark:text-white md:text-3xl  font-burtons">
               Web Developer.
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
@@ -167,86 +122,10 @@ export default function Home() {
                 Are you interested in feedback for your current project? I can
                 give you tips and tricks to level it up.
               </p>
-              {/* <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p> */}
             </div>
           </div>
         </section>
         <section className="py-10">
-          {/* <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
-            </p>
-          </div> */}
-          {/* <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-            <div className="basis-1/3 flex-1 ">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web1}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web2}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web3}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web4}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web5}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web6}
-              />
-            </div>
-          </div> */}
         </section>
       </main>
     </div>
