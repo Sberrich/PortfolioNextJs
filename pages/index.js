f925212e921e4d2569aab6c1a3ea2b486e834ca2
Berrichi Samir <49416974+Sberrich@users.noreply.github.com>
2022-11-01 11:09:37 +0100

    updates wave

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,11 @@ import consulting from "../public/consulting.png";
 import Image from "next/image";
 import Link from "next/link";
 
+
 export default function Home() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
+
+
 
   return (
     <div className={darkMode ? "dark" : ""}>
@@ -25,7 +28,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
-        <section className="min-h-screen">
+        <section >
           <nav className="py-10 mb-12 flex justify-between dark:text-white">
             <h1 className="font-burtons text-xl">sberrich</h1>
             <ul className="flex items-center">
@@ -37,15 +40,14 @@ export default function Home() {
               </li>
               <li>
                 <a
-                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white dark:bg-gray-900 px-4 py-2 rounded-md ml-8 text-l font-burtons"
-                  href="#">
+                  className="bg-gradient-to-r from-cyan-500 to-cyan-500 text-white dark:bg-gray-900 px-4 py-2 rounded-md ml-8 text-l font-burtons" href="../public/resume.pdf" download>
                   Say Hello
                 </a>
               </li>
             </ul>
           </nav>
           <div className="text-center p-10 py-10">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl font-burtons">
+            <h2 className="text-5xl py-2 text-cyan-600 font-medium dark:text-cyan-400 md:text-6xl font-burtons">
              SAMIR BERRICHI
             </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl  font-burtons">
@@ -55,7 +57,7 @@ export default function Home() {
               Freelancer providing services for programming and design content
               needs. Join me down below and let's get cracking!
             </p>
-            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <div className=" flex justify-center gap-16 py-3 text-gray-600  md:text-5xl  dark:text-gray-200">
             <Link href="https://www.linkedin.com/in/samirberrichi/">
               <AiFillLinkedin className="cursor-pointer" />
             </Link>
@@ -69,19 +71,16 @@ export default function Home() {
               <AiFillInstagram className="cursor-pointer" />
             </Link>
           </div>
-            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+            <div className="mx-auto bg-gradient-to-b from-cyan-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-90 md:w-90">
               <Image src={wave} layout="fill" objectFit="cover" />
             </div>
           </div>
         </section>
         <section>
           <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Welcome</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-
-            My name is <span className="text-teal-500"> Samir Berrichi </span> , I'm a Front-end developer based in khouribga Morocco, i have been developed many types of front-ends from well know Matcha "Matching and chating app", hypetype "streaming app for watching movies"
-      
-         
+            <h3 className="text-4xl py-1 dark:text-white font-burtons">Welcome</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200 md:text-xl"> 
+            My name is <span className="text-cyan-500"> Samir Berrichi </span> , I'm a Front-end developer based in khouribga Morocco, i have been developed many types of front-ends from well know Matcha "Matching and chating app", hypetype "streaming app for watching movies"
             </p>
         
           </div>
@@ -95,7 +94,7 @@ export default function Home() {
                 Creating elegant designs suited for your needs following core
                 design theory.
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
+              <h4 className="py-4 text-cyan-600">Design Tools I Use</h4>
               <p className="text-gray-800 py-1">Photoshop</p>
               <p className="text-gray-800 py-1">Illustrator</p>
               <p className="text-gray-800 py-1">Figma</p>
@@ -109,7 +108,7 @@ export default function Home() {
                 Do you have an idea for your next great website? Let's make it a
                 reality.
               </p>
-              <h4 className="py-4 text-teal-600"> I Use</h4>
+              <h4 className="py-4 text-cyan-600"> I Use</h4>
               <p className="text-gray-800 py-1">React JS</p>
               <p className="text-gray-800 py-1">Next JS </p>
               <p className="text-gray-800 py-1">Tailwind</p>
