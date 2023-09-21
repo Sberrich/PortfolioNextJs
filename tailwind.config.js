f5a711114d662c1c350ba6d530530f5067614409
Samir Berrichi <sberrich@e3r7p4.1337.ma>
2023-09-21 21:04:33 +0100

    Update the first section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,20 @@ module.exports = {
     extend: {
       fontFamily: {
         burtons: "burtons",
+        tahu: "tahu",
+        Moranga: "Moranga",
+      },
+      backgroundColor: {
+        'primary-gradient': 'linear-gradient(45deg, #ff5733, #5f0a87)',
+      },
+      keyframes: {
+        rotateBackground: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
       },
     },
+    
   },
   plugins: [],
 };
