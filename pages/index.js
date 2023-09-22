a8aa15c315bc6a89fd38d203a803f54e0cab1106
Samir Berrichi <sberrich@e3r2p6.1337.ma>
2023-09-22 17:39:01 +0100

    add some pic

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,65 @@
 import Head from "next/head";
 import { useState, useEffect } from "react";
 import Image from "next/image";
-
 import { BsSun, BsArrowDownCircle } from "react-icons/bs";
-
 import wave from "../public/profile2.png";
 import sberrich from "../public/wave4.png";
-
 import code from "../public/code.png";
 import design from "../public/design.png";
 import Subject from "../public/Subject.png";
-import cardweb from "../public/card-hello-background.webp";
+import Subject2 from "../public/Subject2.png";
 import consulting from "../public/consulting.png";
+import cardweb from "../public/card-hello-background.webp";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillGithub,
+  AiFillInstagram,
+} from "react-icons/ai";
+
+// Card Component
+const Card = ({ imageSrc, title, subtitle, Desc, children, imgH, imgW }) => (
+  <div className="bg-white dark:bg-primary-black min-w-[300px] rounded-[32px] flex flex-col justify-between relative w-full xl:w-1/2 min-h-[300px] p-[32px] dark:bg-primary-black border-2 dark:bg-gray-800 light-border overflow-hidden">
+    <div className="flex justify-between max-w-full">
+      <div className="flex flex-row items-center gap-5">
+        <div className="max-w-fit">
+          <div className="relative flex items-center justify-center">
+            <div className="absolute w-[50px] h-[50px] bg-[#b5e3fd]/75 rounded-full flex items-center justify-center blur-md"></div>
+            <Image
+              src={imageSrc}
+              width={imgW}
+              height={imgH}
+              decoding="async"
+              className="w-22 h-22 relative top-[-4px] left-[-2px]"
+              loading="lazy"
+            />
+          </div>
+        </div>
+        <div>
+          <p>
+            {title}
+            <span className="font-[Moranga] font-semibold text-xl ml-2">
+              {subtitle}
+            </span>
+          </p>
+          <p className="text-sm text-gray-400">{Desc}</p>
+        </div>
+      </div>
+    </div>
+    <div>{children}</div>
+  </div>
+);
+// LanguageSection Component
+const LanguageSection = ({ languages, languageIndex }) => (
+  <div className="hidden md:block">
+    <div>
+      <h2 className="font-Moranga font-semibold text-2xl">
+        {languages[languageIndex]} ✋
+      </h2>
+    </div>
+    <Image src={wave} width={700} height={700} objectFit="contain" alt="wave" />
+  </div>
+);
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(true);
@@ -36,9 +84,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-gray-200 px-10 dark:bg-gray-900 md:px-20 lg:px-40  duration-300 select-none  animate-fade-in">
-        <section className="">
-          <nav className="py-10 mb-12 flex justify-between dark:text-white">
+      <main className="bg-gray-200 px-10 dark:bg-gray-900 md:px-20 lg:px-40 duration-300 select-none animate-fade-in">
+        <section>
+          <nav className="py-10 mb-12 flex justify-between dark:text-white relative">
+            {/* Background Image with Spin Animation */}
+            <div className="absolute inset-0 z-[-10] animate-spin">
+              <Image
+                src={cardweb}
+                layout="fill"
+                objectFit="cover"
+                alt="background"
+              />
+            </div>
             <div className="flex ml-4 relative items-center justify-center">
               <div className="absolute bg-[#b5e3fd]/30 w-[130px] h-[20px] blur-md"></div>
               <div className="rounded-full flex items-center justify-between">
@@ -50,11 +107,11 @@ export default function Home() {
                 </span>
                 <Image
                   src={Subject}
-                  width={30}
-                  height={30}
+                  width={50}
+                  height={50}
                   objectFit="contain"
                   alt="sberrich"
-                  className="animate-pulse"
+                  className=" animate-pulse"
                 />
                 <span className="text-xl font-bold tracking-wide font-['Tahu'] ">
                   B<span className="opacity-100">e</span>
@@ -80,146 +137,86 @@ export default function Home() {
               </li>
             </ul>
           </nav>
-          <div className="flex flex-col md:flex-row items-center justify-around dark:text-white ">
+          <div className="flex flex-col md:flex-row items-center justify-around dark:text-white">
             {/* Left Side with Four Cards */}
-            <div className="bg-white dark:bg-primary-black min-w-[300px] rounded-[32px] flex flex-col justify-between relative w-full xl:w-1/2 min-h-[300px] p-[32px] dark:bg-primary-black border-2 dark:bg-gray-800 light-border overflow-hidden ">
-              <div className="flex justify-between max-w-full">
-                <div className="flex flex-row items-center gap-5">
-                  <div className="max-w-fit">
-                    <div className="relative flex items-center justify-center">
-                      <div className="absolute w-[50px] h-[50px] bg-[#b5e3fd]/75 rounded-full flex items-center justify-center blur-md"></div>
-                      <Image
-                        alt="profile_alt"
-                        src={sberrich}
-                        width={75}
-                        height={75}
-                        decoding="async"
-                        className="w-22 h-22 relative top-[-4px] left-[-2px]"
-                        loading="lazy"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <p>
-                      Hello, I’m
-                      <span className="font-[Moranga] font-semibold text-xl">
-                        Samir.
-                      </span>
-                    </p>
-                    <p className="text-sm text-secondary">FrontEnd Engineer</p>
-                  </div>
-                </div>
-              </div>
-              <p className="">
-                also known as sberrich, a self-taught
-                <span className="font-[Moranga] text-xl font-semibold m-2">
-                  frontend developer
-                </span>
-                originally from Morocco and residing in Khouribga at the moment.
-                Currently available for freelance work or full-time jobs. When
-                I&rsquo;m not coding or designing, I enjoy reading books,
-                playing football, playing video games, or watching movies.
-                Lately, II&rsquo;ve started hitting the gym to build some
-                strength, burn calories, and II&rsquo;m enjoying it so far.
+            <Card
+              imageSrc={sberrich}
+              imgH={75}
+              imgW={75}
+              title={"Hello, I’m"}
+              subtitle={"Samir Berrichi."}
+              Desc={"FrontEnd Enginer"}
+            >
+              {" "}
+              <p>
+                Hello! I&rsquo;m a passionate web developer with a love for
+                React.js, Next.js, and Tailwind CSS. With 1 year of frontend
+                development experience, I enjoy solving complex problems and
+                creating impactful solutions. When I&rsquo;m not coding, I
+                explore new technologies, work on side projects, and contribute
+                to open-source communities. Let&rsquo;s connect and build
+                something amazing together!
               </p>
-            </div>
+            </Card>
             {/* Right Side with Image */}
-            <div className="md:w-1/2">
-              <div className="hidden md:block">
-                <div>
-                  <h2 className=" font-Moranga font-semibold text-2xl ">
-                    {languages[languageIndex]} ✋
-                  </h2>
-                </div>
-                <Image
-                  src={wave}
-                  width={700}
-                  height={700}
-                  objectFit="contain"
-                  alt="wave"
-                />
-              </div>
-            </div>
+            <LanguageSection
+              languages={languages}
+              languageIndex={languageIndex}
+            />
           </div>
         </section>
         <section>
-          <div className=" flex justify-center gap-8 py-3 text-gray-600  md:text-5xl  dark:text-gray-200"></div>
-          <div className="lg:flex gap-10 mt-28">
-            <div className="bg-white dark:bg-primary-black min-w-[300px] rounded-[32px] flex flex-col justify-between relative w-full xl:w-1/2 min-h-[300px] p-[32px] dark:bg-primary-black border-2 dark:bg-gray-800 light-border overflow-hidden ">
-              <div className="flex justify-between max-w-full">
-                <div className="flex flex-row items-center gap-5">
-                  <div className="max-w-fit">
-                    <div className="relative flex items-center justify-center">
-                      <div className="absolute w-[50px] h-[50px] bg-[#b5e3fd]/75 rounded-full flex items-center justify-center blur-md"></div>
-
-                      <Image
-                        src={design}
-                        width={75}
-                        height={75}
-                        decoding="async"
-                        className="w-22 h-22 relative top-[-4px] left-[-2px]"
-                        loading="lazy"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <h3 className="text-lg font-medium pt-8 pb-2  ">
-                      Beautiful Designs
-                    </h3>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="bg-white dark:bg-primary-black min-w-[300px] rounded-[32px] flex flex-col justify-between relative w-full xl:w-1/2 min-h-[300px] p-[32px] dark:bg-primary-black border-2 dark:bg-gray-800 light-border overflow-hidden ">
-              <div className="flex justify-between max-w-full">
-                <div className="flex flex-row items-center gap-5">
-                  <div className="max-w-fit">
-                    <div className="relative flex items-center justify-center">
-                      <div className="absolute w-[50px] h-[50px] bg-[#b5e3fd]/75 rounded-full flex items-center justify-center blur-md"></div>
-
-                      <Image
-                        src={code}
-                        width={75}
-                        height={75}
-                        decoding="async"
-                        className="w-22 h-22 relative top-[-4px] left-[-2px]"
-                        loading="lazy"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <h3 className="text-lg font-medium pt-8 pb-2  ">
-                      Code your dream project
-                    </h3>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="bg-white dark:bg-primary-black min-w-[300px] rounded-[32px] flex flex-col justify-between relative w-full xl:w-1/2 min-h-[300px] p-[32px] dark:bg-primary-black border-2 dark:bg-gray-800 light-border overflow-hidden ">
-              <div className="flex justify-between max-w-full">
-                <div className="flex flex-row items-center gap-5">
-                  <div className="max-w-fit">
-                    <div className="relative flex items-center justify-center">
-                      <div className="absolute w-[50px] h-[50px] bg-[#b5e3fd]/75 rounded-full flex items-center justify-center blur-md"></div>
-
-                      <Image
-                        src={consulting}
-                        width={75}
-                        height={75}
-                        decoding="async"
-                        className="w-22 h-22 relative top-[-4px] left-[-2px]"
-                        loading="lazy"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <h3 className="text-lg font-medium pt-8 pb-2  ">
-                      Beautiful Designs
-                    </h3>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className="flex justify-center text-center gap-8 py-3 text-gray-600 md:text-5xl dark:text-gray-200"></div>
+          <div className="lg:flex gap-10 mt-28 dark:text-white">
+            <Card
+              imageSrc={design}
+              subtitle="Beautiful Designs"
+              imgH={75}
+              imgW={75}
+            >
+              <p className="py-2">
+                Creating elegant designs suited for your needs following core
+                design theory.
+              </p>
+              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
+            </Card>
+            <Card
+              imageSrc={code}
+              subtitle="Code your dream project"
+              imgH={75}
+              imgW={75}
+            />
+            <Card
+              imageSrc={consulting}
+              subtitle="Consulting Services"
+              imgH={75}
+              imgW={75}
+            />
+          </div>
+          <div className="lg:flex gap-10 mt-28 dark:text-white">
+            <Card imageSrc={Subject2} subtitle="About Me" imgH={75} imgW={75}>
+              <p className=" font-Moranga text-md font-semibold ">
+                Hello! I'm a passionate web developer with a love for React.js,
+                Next.js, and Tailwind CSS. With 1 year of frontend development
+                experience, I enjoy solving complex problems and creating
+                impactful solutions. When I'm not coding, I explore new
+                technologies, work on side projects, and contribute to
+                open-source communities. Let's connect and build something
+                amazing together!
+              </p>
+            </Card>
+            <Card
+              imageSrc={code}
+              subtitle="Code your dream project"
+              imgH={75}
+              imgW={75}
+            />
+            <Card
+              imageSrc={consulting}
+              subtitle="Consulting Services"
+              imgH={75}
+              imgW={75}
+            />
           </div>
         </section>
       </main>
